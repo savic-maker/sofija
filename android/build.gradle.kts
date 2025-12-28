260f131875a3135ffd7d6db8b72012a91b4d2740
savic-maker <savic.srdjanwork@gmail.com>
2025-12-28 17:02:14 +0100

    Update build.gradle.kts

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -4,3 +4,18 @@ allprojects {
         mavenCentral()
     }
 }
+
+// Keep Android build outputs in the Flutter build/ folder (standard Flutter layout)
+rootProject.buildDir = file("../build")
+
+subprojects {
+    // Put each module output under root build dir
+    project.buildDir = file("${rootProject.buildDir}/${project.name}")
+
+    // Ensures :app is evaluated first (avoids some ordering issues in multi-module builds)
+    project.evaluationDependsOn(":app")
+}
+
+tasks.register<Delete>("clean") {
+    delete(rootProject.buildDir)
+}
